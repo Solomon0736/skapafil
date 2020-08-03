33614ad212ecdab9a53ae852bec27c4668e48aef
solomontikabo.musie <Nordmaling10>
2020-08-03 20:00:26 +0200

    Initial commit

---
+++ src/files.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class files {
+    public static void main( String[] args){
+ // hur kan man skapa en fil
+        final Formatter x;
+        try {
+            x = new Formatter(" creat file");
+            System.out.println("it done");
+        }
+        catch (Exception e){
+            System.out.println(" got error");
+        }
+    }
+}
